008ef66958144fef0cfbd016504c8ffaf4f3a50e
eze <chibeze.ochonogor@interswitchng.com>
2023-05-16 22:52:56 +0100

    progress: May 16th 2023

--- PYTHON/counter.py
+++ PYTHON/counter.py
@@ -1,11 +1,22 @@
+# def countFunc(input,treasure) :
+#     if(len(input) == 1) :
+#         if(input[0] == treasure) :
+#             return 1
+#         return 0 
+#     else :
+#         if(input[0] == treasure) :
+#             return 1 + countFunc(input[1:len(input)],treasure)
+#         return countFunc(input[1:len(input)],treasure)
+    
+# print(countFunc("alabalabaa", 'a'))
+
 def countFunc(input,treasure) :
-    if(len(input) == 1) :
-        if(input[0] == treasure) :
-            return 1
-        return 0 
-    else :
+        if(len(input) == 0) :
+              return 0
         if(input[0] == treasure) :
             return 1 + countFunc(input[1:len(input)],treasure)
         return countFunc(input[1:len(input)],treasure)
-    
-print(countFunc("alabalabaa", 'a'))
+
+print(countFunc("alealeale", 'p'))
+
+        
