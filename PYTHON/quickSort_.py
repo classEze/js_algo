7ecdcee7ad7f761fe34ed113ff69e214fcca6c20
eze <chibeze.ochonogor@interswitchng.com>
2023-06-06 23:18:59 +0100

    Progress June 6th 2023

--- PYTHON/quickSort_.py
+++ PYTHON/quickSort_.py
@@ -1,12 +1,14 @@
 def quick_sort(arr):
     if len(arr) <= 1:
         return arr
+    return sorter(arr, 0, len(arr) - 2, len(arr) - 1)
 
-    def sorter(arr, leftPointer, rightPointer, pivotIndex):
-        if leftPointer > rightPointer:
+
+def sorter(arr, leftPointer, rightPointer, pivotIndex):
+        if leftPointer >= rightPointer:
             return
 
-        while leftPointer < rightPointer:
+        while True:
             while arr[leftPointer] < arr[pivotIndex]:
                 leftPointer += 1
 
@@ -19,17 +21,16 @@ def quick_sort(arr):
                 temp = arr[leftPointer]
                 arr[leftPointer] = arr[rightPointer]
                 arr[rightPointer] = temp
+                leftPointer += 1
 
         temp = arr[leftPointer]
         arr[leftPointer] = arr[pivotIndex]
         arr[pivotIndex] = temp
 
-        sorter(arr, leftPointer + 1, len(arr) - 2, len(arr) - 1)
-        sorter(arr, 0, leftPointer - 2, leftPointer - 1)
+        sorter(arr, leftPointer + 1, len(arr) - 2, len(arr) - 1) # right side
+        sorter(arr, 0, leftPointer - 2, leftPointer - 1) # left side
 
-    return sorter(arr, 0, len(arr) - 2, len(arr) - 1)
-
-
-myArr = [2, 34, 5, 7]
+        
+myArr = [2000,3,100,2, 5,1,123]
 quick_sort(myArr)
 print(myArr)
